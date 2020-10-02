16285e112a4c4b2025432a0642a00a89ac0999dc
aakash2410 <aakash.sangani2410@gmail.com>
2020-10-02 15:44:02 +0530

    Generic Views

--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -5,12 +5,8 @@ from . import views
 app_name = 'pollsapp'
 
 urlpatterns = [
-    # ex: /pollsapp/
-    path('', views.index, name='index'),
-    # ex: /pollsapp/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /pollsapp/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /pollsapp/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
