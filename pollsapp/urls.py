31cd6be1280582e19a79af127fade02748993bdb
aakash2410 <aakash.sangani2410@gmail.com>
2020-10-02 11:07:54 +0530

    urls and views set up

--- pollsapp/urls.py
+++ pollsapp/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pollsapp'
+
 urlpatterns = [
+    # ex: /pollsapp/
     path('', views.index, name='index'),
+    # ex: /pollsapp/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /pollsapp/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /pollsapp/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
